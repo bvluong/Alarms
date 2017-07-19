435cf11455d27c7e04f65e60c93c86be8e23478b
bvluong <bryantvinluong@gmail.com>
2017-07-19 11:49:38 -0700

    Add upvote/downvote specs to Alarm model

--- spec/models/alarm_spec.rb
+++ spec/models/alarm_spec.rb
@@ -21,11 +21,37 @@ RSpec.describe Alarm, type: :model do
 
     it "expects default value to be 0" do
       Alarm.create!(title: "Ambient Noise")
-      alarm2 = Alarm.find_by_title("Ambient Noise")
-      expect(alarm2.value).to eq(0)
+      alarm = Alarm.find_by_title("Ambient Noise")
+      expect(alarm.value).to eq(0)
+    end
+end
+
+  describe "Upvote Alarm" do
+    it "should increase value" do
+      Alarm.create!(title: "Beeping")
+      alarm = Alarm.find_by_title("Beeping")
+      alarm.upvote
+      expect(alarm.value).to eq(1)
+    end
+  end
+
+  describe "Downvote Alarm" do
+    it "should not decrease below 0" do
+      Alarm.create!(title: "Noise")
+      alarm = Alarm.find_by_title("Noise")
+      alarm.downvote
+      expect(alarm.value).to eq(0)
     end
 
-end
+    it "should decrease value" do
+      Alarm.create!(title: "Ocean Waves")
+      alarm = Alarm.find_by_title("Ocean Waves")
+      5.times { alarm.upvote }
+      alarm.downvote
+      expect(alarm.value).to eq(4)
+    end
+  end
+
 
 it { should validate_presence_of(:title) }
 
