36ae2b22ad63a071865d173db91ff2c7c401fb9b
bvluong <bryantvinluong@gmail.com>
2017-07-19 10:54:35 -0700

    Update specs for User upvotes alarm spec

--- spec/features/user_upvotes_alarm_spec.rb
+++ spec/features/user_upvotes_alarm_spec.rb
@@ -4,7 +4,8 @@ feature 'user upvotes an alarm' do
   scenario 'successfully' do
     visit root_path
 
-    click_on "Upvote"
+    find('ul > li:first-child')
+    click_button('Upvote', match: :first)
     expect(page).to have_css ".alarms li h4", text: "1"
   end
 end
--- spec/models/alarm_spec.rb
+++ spec/models/alarm_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Alarm, type: :model do
   describe "create new Alarm" do
-  it "successfully" do
-    Alarm.create!(title: "Beeping")
-    alarm = Alarm.find_by_title("Beeping")
-    expect(alarm.title).to eq("Beeping")
-  end
+    it "successfully" do
+      Alarm.create!(title: "Beeping")
+      alarm = Alarm.find_by_title("Beeping")
+      expect(alarm.title).to eq("Beeping")
+    end
+    
 end
 
 it { should validate_presence_of(:title) }
